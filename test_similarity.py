f35ec9749d32cb4f9f505327e7ad55f2fb71aba9
Tran Thi Hue <huetran1611@yahoo.com.vn>
2025-08-24 08:48:55 +0700

    one visit 50 random

--- test_similarity.py
+++ test_similarity.py
@@ -472,7 +472,7 @@ for txt_file in txt_files:
         # log_file_path = os.path.join(log_folder, log)
         # log_file = open(log_file_path, 'w')
         # sys.stdout = log_file
-        Data.read_data(txt_file)
+        Data.read_data_random(txt_file)
         result = []
         run_time = []
         avg = 0
